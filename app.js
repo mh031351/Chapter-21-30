cb8fef7b16a7e70dcda4f7cf61fae867e69b7650
mh031351 <mh031351>
2021-11-10 05:08:48 -0800

    Work Complete

---
+++ app.js
@@ -0,0 +1,19 @@
+alert("Hello world")
+
+///// chapter number 21-25 /////
+
+var firstName = prompt("Enter your firstname ");
+var lastName = prompt("Enter your lastname");
+var firstName, lastName, fullName;
+firstName = 'Hussain';
+lastName = 'Khanzada';
+fullName = 'firstName' + 'lastName';
+
+
+var mob=prompt("Enter your favourite mobile phone model");
+var len= mob.length;
+document.write("Lenght of the string: "+ len); 
+document.write(mob)
+
+var str=prompt("Enter a index number");
+document.write(str.slice(-1));
